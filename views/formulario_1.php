7764c02b107cc43ad9cfb596da70f94142e2385b
Camilo Castellar <camilocastelar3@gmail.com>
2022-06-29 12:32:53 -0500

    fix structure

--- views/formulario_1.php
+++ views/formulario_1.php
@@ -1,5 +1,5 @@
 <?php 
-    require 'src/forms/nota_final.php';
+    require '../src/forms/nota_final.php';
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/formulario_1_style.css">
+    <link rel="stylesheet" href="../css/formulario_1_style.css">
     <title>FORMULARIO 1</title>
 </head>
 <body>
